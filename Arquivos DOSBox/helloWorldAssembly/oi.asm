8d0facd919f7bddcfaec46bd039118bb4cc51dfc
mrtuckie <arthur.lorencini@gmail.com>
2020-09-23 11:57:38 -0300

    versão sem comentários

---
+++ Arquivos DOSBox/helloWorldAssembly/oi.asm
@@ -0,0 +1,27 @@
+segment code
+..start:
+	mov ax,data 
+	mov ds,ax 
+	mov ax,stack 
+	mov ss,ax 
+	mov sp,stacktop 
+
+
+
+	mov ah,9 
+	mov dx,mensagem 
+	int 21h 
+	mov ah,4ch 
+	int 21h 
+	
+segment data
+CR	equ	0dh 
+LF	equ 0ah 
+
+
+mensagem db 'Oi, salve familia',CR,LF,'$' 
+
+segment stack stack 
+	resb 256 
+	
+stacktop: ; 
